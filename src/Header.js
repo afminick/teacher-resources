779121c12c7860650648595f236391154d05878d
afminick <afminick@gmail.com>
2019-08-17 21:28:09 +0700

    Progress on navbar collapse

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
+function responsiveMenu(props) {
+
+    const openMenu = <ul>
+    <li><a href="https://vigilant-lichterman-f14f98.netlify.com/">Tic-Tac-Toe</a></li>
+    <li><a href="https://goofy-montalcini-2ef709.netlify.com/">Hangman</a></li>
+    <li>Home</li>
+    <li>Home</li>
+</ul>
+    return (
+        <div className="navbar">
+            <button className="menuOpenButton">X</button>
+            {props.menuStatus ? openMenu : null}
+        </div>
+    )
+}
+
 function Header(props) {
     return (
         <header>
@@ -7,14 +23,8 @@ function Header(props) {
                 <h1>Online ESL Teacher Resources</h1>
                 <h3>Your source for games, lesson plans, and more!</h3>
             </div>
-            <div className="navbar">
-                <ul>
-                    <li><a href="https://vigilant-lichterman-f14f98.netlify.com/">Tic-Tac-Toe</a></li>
-                    <li><a href="https://goofy-montalcini-2ef709.netlify.com/">Hangman</a></li>
-                    <li>Home</li>
-                    <li>Home</li>
-                </ul>
-            </div>
+            {responsiveMenu(props)}
+            
             
         </header>
     )
