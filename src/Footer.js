24f9d86739cc28371c320aca73a92eb7936b6bd6
afminick <afminick@gmail.com>
2019-09-10 16:52:32 +0700

    Took off footer contact size change

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Footer() {
     return(
         <footer>
-            <h3>AMWD</h3>
+            <p>AMWD</p>
             <div id="footerContact">
-                <a className="contactLink" href="#"><FontAwesomeIcon icon="envelope" size="2x" /></a>
-                <a className="contactLink" href="#"><FontAwesomeIcon icon={['fab', 'facebook-square']} size="2x" /></a>
-                <a className="contactLink" href="#"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x" /></a>
-                <a className="contactLink" href="#"><FontAwesomeIcon icon={['fab', 'twitter-square']} size='2x'/></a>
+                <a href="#"><FontAwesomeIcon icon="envelope"  size="1.5" /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'facebook-square']}  size="1.5" /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'instagram']}  size="1.5" /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'twitter-square']} size="1.5" /></a>
             </div>
         </footer>
     )
