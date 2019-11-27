60ddb612bdfa63d4ba61b7e8bb99638e7e19aad0
afminick <afminick@gmail.com>
2019-11-27 17:51:41 +0900

    Fixed a small key error

--- src/pages/games/tictactoe/GameSquare.js
+++ src/pages/games/tictactoe/GameSquare.js
@@ -3,7 +3,7 @@ import React from 'react'
 function GameSquare(props) {
     const letterSize = props.word.length > 8 ? {fontSize: '0.7em'} : {fontSize: '1em'};
     return (
-        <div className='tictactoeSquare' id={props.key}>
+        <div className='tictactoeSquare' id={props.number}>
             <h3 style={letterSize} className='squareWord'>{props.word}</h3>
             <h2 className='squareMark'>{props.mark}</h2>
             <div className='buttons'>
